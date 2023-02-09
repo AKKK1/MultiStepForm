b6f2886828bc871663c2aa6ee4ecc365431e5f83
AKKK1 <akakichachava1@gmail.com>
2023-02-09 06:25:28 +0400

    axali

--- src/components/multi-step-form/styled.jsx
+++ src/components/multi-step-form/styled.jsx
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 
-
 export const StepForm = styled.div`
-background-color: hsl(0, 0%, 100%);
-display: flex;
-gap: 12px;
+  background-color: hsl(0, 0%, 100%);
+  display: flex;
+  gap: 12px;
 
-padding: 12px;
-min-height: 60vh;
-border-radius: 16px;
-min-width: 90vw ;
-box-shadow: 0 5px 10px 10px rgba(0, 0, 0, 0.05);
-
-`
+  padding: 12px;
+  min-height: 90vh;
+  border-radius: 16px;
+  min-width: 80vw;
+  box-shadow: 0 5px 10px 10px rgba(0, 0, 0, 0.05);
+`;
--- src/components/sidebar/styled.jsx
+++ src/components/sidebar/styled.jsx
@@ -1,66 +1,55 @@
-import styled,{css} from "styled-components";
-import SidebarBg from '../../images/bg-sidebar-desktop.svg'
+import styled, { css } from "styled-components";
+import SidebarBg from "../../images/bg-sidebar-desktop.svg";
 
 export const SideBar = styled.div`
-display: flex;
-flex-direction: column;
-gap: 24px;
-background-image: url(${SidebarBg});
-position: center;
-background-repeat: no-repeat;
-background-size: cover;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  background-image: url(${SidebarBg});
+  position: left;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 70%;
 
-color: white;
-padding: 24px;
-
-width: 40%;
-border-radius: 8px;
-
-`
+  color: white;
+  padding: 24px;
+  width: 40%;
+  border-radius: 8px;
+`;
 export const ItemNumber = styled.span`
-width: 32px;
-height: 32px;
-border: 1px solid white;
-border-radius: 999px;
-display: flex;
-align-items: center;
-justify-content: center;
-
-`
+  width: 32px;
+  height: 32px;
+  border: 1px solid white;
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
 
-display: flex;
-align-items: center;
-gap: 8px;
-
-${ItemNumber} {
-
-    ${
-    ({isActive})=> isActive && css`
+  ${ItemNumber} {
+    ${({ isActive }) =>
+      isActive &&
+      css`
         background-color: var(--Magnolia);
         color: var(--Marine-blue);
-        border-color: var(--Magnolia);`
-}
-}
+        border-color: var(--Magnolia);
+      `}
+  }
+`;
 
-
-
-`
-
-
-export const ItemBody = styled.div`
-
-
-`
+export const ItemBody = styled.div``;
 
 export const Subtitle = styled.h5`
-opacity: 0.75;
+  opacity: 0.75;
 
-font-size: 13px;
-`
+  font-size: 13px;
+`;
 
 export const Title = styled.h2`
-font-weight: 500;
-
-`
+  font-weight: 500;
+`;
